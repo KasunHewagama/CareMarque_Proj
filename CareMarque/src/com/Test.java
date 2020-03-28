0c2746c4bf91fa6134f79e74520e3356bb1ece08
KasunHewagama <kghewagama1997@gmail.com>
2020-03-28 14:03:43 +0530

    Initialization of CareMarque

---
+++ CareMarque/src/com/Test.java
@@ -0,0 +1,23 @@
+package com;
+
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
+
+@Path("/Test")
+public class Test {
+	
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_PLAIN)
+	
+	public String Text()
+	{
+		return "Hello World";
+	}
+
+}
