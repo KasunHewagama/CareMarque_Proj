bcd29e240541b1959c18f5d957c801e8a9e97f28
KasunHewagama <kghewagama1997@gmail.com>
2020-04-04 23:43:39 +0530

    appointment service...!

---
+++ CareMarque/src/com/appointment/service/A.java
@@ -0,0 +1,11 @@
+package com.appointment.service;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test...!");
+
+	}
+
+}
