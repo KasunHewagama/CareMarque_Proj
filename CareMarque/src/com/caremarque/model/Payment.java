fe9f3c2671c44d76efe757a04f4ddb7625e14dbd
YuvinNP <yuvinnimnaka1998@gmail.com>
2020-04-05 11:22:50 +0530

    formatted some classes

--- CareMarque/src/com/caremarque/model/Payment.java
+++ CareMarque/src/com/caremarque/model/Payment.java
@@ -1,7 +1,7 @@
 package com.caremarque.model;
 
 public class Payment {
-	
+
 	private String paymentId;
 	private String patientId;
 	private String patientName;
@@ -11,67 +11,81 @@ public class Payment {
 	private String appoinmentDate;
 	private String paymentDate;
 	private double amount;
-	
+
 	public Payment() {
-		
+
 	}
-	
+
 	public String getPaymentId() {
 		return paymentId;
 	}
-	
+
 	public void setPaymentId(String paymentId) {
 		this.paymentId = paymentId;
 	}
+
 	public String getPatientId() {
 		return patientId;
 	}
+
 	public void setPatientId(String patientId) {
 		this.patientId = patientId;
 	}
+
 	public String getPatientName() {
 		return patientName;
 	}
+
 	public void setPatientName(String patientName) {
 		this.patientName = patientName;
 	}
+
 	public String getAppointmentId() {
 		return appointmentId;
 	}
+
 	public void setAppointmentId(String appointmentId) {
 		this.appointmentId = appointmentId;
 	}
+
 	public String getDoctorId() {
 		return doctorId;
 	}
+
 	public void setDoctorId(String doctorId) {
 		this.doctorId = doctorId;
 	}
+
 	public String getHospitalId() {
 		return hospitalId;
 	}
+
 	public void setHospitalId(String hospitalId) {
 		this.hospitalId = hospitalId;
 	}
+
 	public String getAppoinmentDate() {
 		return appoinmentDate;
 	}
+
 	public void setAppoinmentDate(String appoinmentDate) {
 		this.appoinmentDate = appoinmentDate;
 	}
+
 	public String getPaymentDate() {
 		return paymentDate;
 	}
+
 	public void setPaymentDate(String paymentDate) {
 		this.paymentDate = paymentDate;
 	}
+
 	public double getAmount() {
 		return amount;
 	}
+
 	public void setAmount(double amount) {
 		this.amount = amount;
 	}
-	
-	
 
 }
