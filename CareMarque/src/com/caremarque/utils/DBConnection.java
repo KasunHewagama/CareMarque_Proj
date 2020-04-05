78814f5e9f3d553d15e952690eb0bbcd4ba632dd
user <user@DESKTOP-RJ5NGBK>
2020-04-05 15:36:30 +0530

    Implemented DBConnection class

--- CareMarque/src/com/caremarque/utils/DBConnection.java
+++ CareMarque/src/com/caremarque/utils/DBConnection.java
@@ -1,6 +1,28 @@
 package com.caremarque.utils;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DBConnection {
 
 	//TODO:Implement DB class with phpmyadmin
+	
+public static Connection connect() {
+		
+		Connection con = null;
+		
+		try {
+			
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/healthcare", "root", "root");
+			
+		}catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+		
+		return con;
+		
+	}
 }
