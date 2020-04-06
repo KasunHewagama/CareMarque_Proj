1597d34c3ecae2c3fb0d0490f4d9e10b5b419686
KasunHewagama <kghewagama1997@gmail.com>
2020-04-06 13:04:38 +0530

    adding new fields for appointment...!

---
+++ CareMarque/src/com/caremarque/resources/AppointmentResource.java
@@ -0,0 +1,21 @@
+package com.caremarque.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/Appointment")
+public class AppointmentResource {
+	
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_PLAIN)
+	
+	public String getAppointments() {
+		// TODO Auto-generated constructor stub
+		return "Appointment Resource Called...!";
+	}
+	
+
+}
--- CareMarque/src/com/caremarque/service/appoinment/AppointmentService.java
+++
@@ -1,5 +0,0 @@
-package com.caremarque.service.appoinment;
-
-public class AppointmentService {
-
-}
