c3a1bfc0ddf20a9fb834d3934ee6d611697a5c5f
KasunHewagama <kghewagama1997@gmail.com>
2020-04-09 09:09:06 +0530

    initialized crud...!

--- CareMarque/src/com/caremarque/service/appointment/IAppointmentService.java
+++ CareMarque/src/com/caremarque/service/appointment/IAppointmentService.java
@@ -1,5 +1,19 @@
 package com.caremarque.service.appointment;
 
+import java.util.ArrayList;
+
+import com.caremarque.model.Appointment;
+
 public interface IAppointmentService {
 
+	public String createAppointment(Appointment a);
+	
+	public Appointment getAppointment(String appointmentId);
+	
+	public ArrayList<Appointment> getAppointments();
+	
+	public Appointment updateAppointment(String appointmentid, Appointment id);
+	
+	public void cancelAppointment(String appointmnetId);
+	
 }
