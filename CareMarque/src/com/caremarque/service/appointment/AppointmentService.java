6872801c527efdb9eca1d2613884ee1fa648c89c
KasunHewagama <kghewagama1997@gmail.com>
2020-04-09 09:10:33 +0530

    initialized appointmentService...!

--- CareMarque/src/com/caremarque/service/appointment/AppointmentService.java
+++ CareMarque/src/com/caremarque/service/appointment/AppointmentService.java
@@ -1,5 +1,41 @@
 package com.caremarque.service.appointment;
 
-public class AppointmentService {
+import java.util.ArrayList;
+
+import com.caremarque.model.Appointment;
+
+public class AppointmentService implements IAppointmentService{
+
+	@Override
+	public String createAppointment(Appointment a) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Appointment getAppointment(String appointmentId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Appointment> getAppointments() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Appointment updateAppointment(String appointmentid, Appointment id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void cancelAppointment(String appointmnetId) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 
 }
