f625a78e11587e38044c535b2bbfa8dba7ca5d7f
KaushalyaMadushani <mkaushalya96@gmail.com>
2020-04-10 09:12:46 +0530

    implement IPatientService interface class with methods

--- CareMarque/src/com/caremarque/service/patient/IPatientService.java
+++ CareMarque/src/com/caremarque/service/patient/IPatientService.java
@@ -1,5 +1,19 @@
 package com.caremarque.service.patient;
 
+import java.util.ArrayList;
+
+import com.caremarque.model.Patient;
+
 public interface IPatientService {
 
+	public String registerPatient(Patient patient);
+	
+	public Patient getPatient(String patientId);
+	
+	public ArrayList<Patient> getPatients();
+	
+	public Patient updatePatientDetails(String patientId, Patient patient);
+	
+	public String deletePatient(String patientId);
+	
 }
