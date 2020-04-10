d83c6716691ce360411ca5edea222b592e51f1a2
KasunHewagama <kghewagama1997@gmail.com>
2020-04-11 00:41:20 +0530

    HospitaResource displayed...!

--- CareMarque/src/com/caremarque/resources/HospitalResource.java
+++ CareMarque/src/com/caremarque/resources/HospitalResource.java
@@ -1,5 +1,19 @@
 package com.caremarque.resources;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/Hospital")
 public class HospitalResource {
+	
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getAppointments() {
+		// TODO Auto-generated constructor stub
+		return "Hospital Resource Called...!";
+	}
 
 }
