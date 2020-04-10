2501e3e4163fa548fb204ce30032249530268707
YuvinNP <yuvinnimnaka1998@gmail.com>
2020-04-10 21:13:32 +0530

    formated and added values for constant.java file

--- CareMarque/src/com/caremarque/utils/Constants.java
+++ CareMarque/src/com/caremarque/utils/Constants.java
@@ -1,35 +1,22 @@
 package com.caremarque.utils;
 
 public class Constants {
-//TODO: Declare some constant values used in project
+	//TODO: Declare some constant values used in project
 	
+	//Constant Values for column indexes
 	public static final int COLUMN_INDEX_ONE = 1;
-	
 	public static final int COLUMN_INDEX_TWO = 2;
-	
 	public static final int COLUMN_INDEX_THREE = 3;
-	
 	public static final int COLUMN_INDEX_FOUR = 4;
-	
 	public static final int COLUMN_INDEX_FIVE = 5;
-	
 	public static final int COLUMN_INDEX_SIX = 6;
-	
 	public static final int COLUMN_INDEX_SEVEN = 7;
-	
 	public static final int COLUMN_INDEX_EIGHT = 8;
-	
 	public static final int COLUMN_INDEX_NINE = 9;
-	
 	public static final int COLUMN_INDEX_TEN = 10;
-	
 	public static final int COLUMN_INDEX_ELEVEN = 11;
-	
 	public static final int COLUMN_INDEX_TWELVE = 12;
-	
 	public static final int COLUMN_INDEX_THIRTEEN = 13;
-	
 	public static final int COLUMN_INDEX_FOURTEEN = 14;
-	
 	public static final int COLUMN_INDEX_FIFTEEN = 15;
 }
