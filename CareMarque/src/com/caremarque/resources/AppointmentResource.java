bbf6b28500960322d64a8c023e8ef6ea279b3b4f
KasunHewagama <kghewagama1997@gmail.com>
2020-04-11 13:13:38 +0530

    minor changes in AppointmentResources...!

--- CareMarque/src/com/caremarque/resources/AppointmentResource.java
+++ CareMarque/src/com/caremarque/resources/AppointmentResource.java
@@ -1,12 +1,31 @@
 package com.caremarque.resources;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import com.caremarque.model.Appointment;
+import com.caremarque.service.appointment.AppointmentService;
+
 @Path("/Appointment")
 public class AppointmentResource {
+	
+	AppointmentService as = new AppointmentService();
+	
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String createAppointment() {
+		Appointment appointment = new Appointment();
+		
+		return as.createAppointment(appointment);
+		
+	}
+		
 	
 	@GET
 	@Path("/")
