c65c95fc65f4cd80899ee8c592859d01b1909e85
KasunHewagama <kghewagama1997@gmail.com>
2020-04-11 13:52:27 +0530

    initialized IHospitalService interface...!

--- CareMarque/src/com/caremarque/service/hospital/IHospitalService.java
+++ CareMarque/src/com/caremarque/service/hospital/IHospitalService.java
@@ -1,5 +1,18 @@
 package com.caremarque.service.hospital;
 
+import java.util.ArrayList;
+import com.caremarque.model.Hospital;
+
 public interface IHospitalService {
+	
+	public String createHospital(Hospital h);
+	
+	public Hospital getHospital(String hospitalId);
+	
+	public ArrayList<Hospital> getHospitals();
+	
+	public Hospital updateHospital(String hospitalid,Hospital id);
+	
+	public void cancelHospital(String hospitalId);
 
 }
