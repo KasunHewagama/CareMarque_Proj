3a0d22286a7c23520f393f1e5ccd619698a790e3
KasunHewagama <kghewagama1997@gmail.com>
2020-04-14 11:04:57 +0530

    HospitaResourse.java file commented...!

--- CareMarque/src/com/caremarque/resources/HospitalResource.java
+++ CareMarque/src/com/caremarque/resources/HospitalResource.java
@@ -5,15 +5,16 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/Hospital")
+//@Path("/Hospital")
 public class HospitalResource {
 	
-	@GET
-	@Path("/")
-	@Produces(MediaType.TEXT_PLAIN)
-	public String getAppointments() {
-		// TODO Auto-generated constructor stub
-		return "Hospital Resource Called...!";
-	}
+	/*
+	 * @GET
+	 * 
+	 * @Path("/")
+	 * 
+	 * @Produces(MediaType.TEXT_PLAIN) public String getAppointments() { // TODO
+	 * Auto-generated constructor stub return "Hospital Resource Called...!"; }
+	 */
 
 }
