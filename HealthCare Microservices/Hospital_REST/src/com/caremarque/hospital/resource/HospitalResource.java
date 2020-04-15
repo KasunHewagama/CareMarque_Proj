7064a5fd7e39aa3745f591b2bea70ce2b38b6e4e
mailg <mailg@DESKTOP-TJO0578>
2020-04-15 18:03:03 +0530

    HospitalResource DELETE andd PUT method

--- HealthCare Microservices/Hospital_REST/src/com/caremarque/hospital/resource/HospitalResource.java
+++ HealthCare Microservices/Hospital_REST/src/com/caremarque/hospital/resource/HospitalResource.java
@@ -1,9 +1,11 @@
 package com.caremarque.hospital.resource;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -54,7 +56,22 @@ public class HospitalResource {
 		return as.getHospitals();
 	}
 	
+	@PUT
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_HTML)
+	public String putHospital() {
+		return null;
+	}
 	
+	
+	@DELETE
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_XML)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String deleteHospital(String hospital) {
+		return null;
+	}
 	
 	
 	
