a9c074b9e2442cf44ec63befbc0ed9389781c9b4
mailg <mailg@DESKTOP-TJO0578>
2020-04-15 23:32:22 +0530

    minor changes.

--- HealthCare Microservices/Hospital_REST/src/com/caremarque/hospital/model/Hospital.java
+++ HealthCare Microservices/Hospital_REST/src/com/caremarque/hospital/model/Hospital.java
@@ -9,57 +9,69 @@ public class Hospital {
 	private String address;
 	private String Open_Hours;
 	private String Close_Hours;
-	
-	
-	
+
 	public Hospital() {
 		// TODO Auto-generated constructor stub
 	}
+
 	public String getHospitalId() {
 		return hospitalId;
 	}
+
 	public void setHospitalId(String hospitalId) {
 		this.hospitalId = hospitalId;
 	}
+
 	public String getHospitalName() {
 		return hospitalName;
 	}
+
 	public void setHospitalName(String hospitalName) {
 		this.hospitalName = hospitalName;
 	}
+
 	public String getPhone() {
 		return phone;
 	}
+
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
+
 	public String getRegNo() {
 		return regNo;
 	}
+
 	public void setRegNo(String regNo) {
 		this.regNo = regNo;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
 	public String getOpen_Hours() {
 		return Open_Hours;
 	}
+
 	public void setOpen_Hours(String open_Hours) {
 		Open_Hours = open_Hours;
 	}
+
 	public String getClose_Hours() {
 		return Close_Hours;
 	}
+
 	public void setClose_Hours(String close_Hours) {
 		Close_Hours = close_Hours;
 	}
-	
-	public Hospital(String hospitalId, String hospitalName, String phone, String regNo, String address,String Open_Hours,String Close_Hours)
-	{
+
+	public Hospital(String hospitalId, String hospitalName, String phone, String regNo, String address,
+			String Open_Hours, String Close_Hours) {
 		super();
 		this.hospitalId = hospitalId;
 		this.hospitalName = hospitalName;
@@ -68,10 +80,7 @@ public class Hospital {
 		this.address = address;
 		this.Open_Hours = Open_Hours;
 		this.Close_Hours = Close_Hours;
-		
-		
+
 	}
-	
-	
 
 }
