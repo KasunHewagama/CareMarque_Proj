81b830781c85a8f7cfff89462b6b7f2b7a7ebd53
TaneeshaDew <tdsuwandarachchi98@gmail.com>
2020-04-15 23:57:19 +0530

    getters and setters are created.

--- HealthCare Microservices/Hospital_REST/src/com/caremarque/hospital/model/Hospital.java
+++ HealthCare Microservices/Hospital_REST/src/com/caremarque/hospital/model/Hospital.java
@@ -9,48 +9,61 @@ public class Hospital {
 	private String address;
 	private String Open_Hours;
 	private String Close_Hours;
+
 	public String getHospitalId() {
 		return hospitalId;
 	}
+
 	public void setHospitalId(String hospitalId) {
 		this.hospitalId = hospitalId;
 	}
+
 	public String getHospitalName() {
 		return hospitalName;
 	}
+
 	public void setHospitalName(String hospitalName) {
 		this.hospitalName = hospitalName;
 	}
+
 	public String getPhone() {
 		return phone;
 	}
+
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
+
 	public String getRegNo() {
 		return regNo;
 	}
+
 	public void setRegNo(String regNo) {
 		this.regNo = regNo;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
 	public String getOpen_Hours() {
 		return Open_Hours;
 	}
+
 	public void setOpen_Hours(String open_Hours) {
 		Open_Hours = open_Hours;
 	}
+
 	public String getClose_Hours() {
 		return Close_Hours;
 	}
+
 	public void setClose_Hours(String close_Hours) {
 		Close_Hours = close_Hours;
 	}
 
-	
 }
