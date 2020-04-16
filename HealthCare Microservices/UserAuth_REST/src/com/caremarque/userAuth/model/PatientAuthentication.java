f5c7c558e091806838b020fa525a0ce6eca2a5be
KaushalyaMadushani <mkaushalya96@gmail.com>
2020-04-17 00:58:55 +0530

    create PatientAuthentication class

---
+++ HealthCare Microservices/UserAuth_REST/src/com/caremarque/userAuth/model/PatientAuthentication.java
@@ -0,0 +1,40 @@
+package com.caremarque.userAuth.model;
+
+public class PatientAuthentication {
+
+	private int patientAuthId;
+	private String userName;
+	private String password;
+	
+	public PatientAuthentication() {
+		super();
+	}
+
+	public int getPatientAuthId() {
+		return patientAuthId;
+	}
+
+	public void setPatientAuthId(int patientAuthId) {
+		this.patientAuthId = patientAuthId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+	
+}
