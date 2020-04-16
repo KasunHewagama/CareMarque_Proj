32f8d709d0b1dadea55cca123e32d65d6af7891b
KaushalyaMadushani <mkaushalya96@gmail.com>
2020-04-17 01:20:41 +0530

    implement IUserAuthService interface

--- HealthCare Microservices/UserAuth_REST/src/com/caremarque/userAuth/service/IUserAuthService.java
+++ HealthCare Microservices/UserAuth_REST/src/com/caremarque/userAuth/service/IUserAuthService.java
@@ -1,5 +1,10 @@
 package com.caremarque.userAuth.service;
 
+import java.util.List;
+
+import com.caremarque.userAuth.model.PatientAuthentication;
+
 public interface IUserAuthService {
 
+	public List<PatientAuthentication> checkPatientDetails();
 }
