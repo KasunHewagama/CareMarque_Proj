5b6a18c3a5cb3c25c890ec4da9640bcb930a6550
KaushalyaMadushani <mkaushalya96@gmail.com>
2020-04-17 01:18:06 +0530

    Implement method checkPatientDetails() in UserAuthService class

--- HealthCare Microservices/UserAuth_REST/src/com/caremarque/userAuth/service/UserAuthService.java
+++ HealthCare Microservices/UserAuth_REST/src/com/caremarque/userAuth/service/UserAuthService.java
@@ -5,38 +5,56 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-import com.caremarque.userAuth.model.userAuth;
+import com.caremarque.userAuth.model.PatientAuthentication;
 import com.caremarque.userAuth.utils.DBConnection;
 
 public class UserAuthService {
 
+	public static final Logger log = Logger.getLogger(UserAuthService.class.getName());
+	
 	private static Connection con = null;
+	
 	private static PreparedStatement preparedStmt = null;
+	
 	ResultSet resultSet = null;
 	
 	
-//	public List<userAuth> checkPatientDetails(){
-//		
-//		List<userAuth> patientList = new ArrayList<userAuth>();
-//		
-//		try {
-//			
-//			con = DBConnection.getDBConnection();
-//			
-//			String query = "SELECT * FROM patient";
-//			
-//			preparedStmt = con.prepareStatement(query);
-//			
-//			while(resultSet.next()) {
-//				
-//				userAuth patientAuth = new userAuth();
-//				
-//				patientAuth.setUserAuthId(resultSet.getInt(1));
-//				patientAuth.set
-//			}
-//		}
-//		
-//		return patientList;
-//	}
+	public List<PatientAuthentication> checkPatientDetails(){
+		
+		List<PatientAuthentication> patientList = new ArrayList<PatientAuthentication>();
+		
+		try {
+			
+			con = DBConnection.getDBConnection();
+			
+			String query = "SELECT patientId, email, password FROM patient";
+			
+			preparedStmt = con.prepareStatement(query);
+			
+			while(resultSet.next()) {
+				
+				PatientAuthentication patientAuth = new PatientAuthentication();
+				
+				patientAuth.setPatientAuthId(resultSet.getInt("patientId"));
+				System.out.println("authId : " + resultSet.getInt("patientId"));
+				
+				patientAuth.setUserName(resultSet.getString("email"));
+				System.out.println("authemail : " + resultSet.getInt("email"));
+
+				patientAuth.setPassword(resultSet.getString("password"));
+				System.out.println("authpassword : " + resultSet.getInt("password"));
+
+				patientList.add(patientAuth);
+			}
+			System.out.println("Data retrived from DB");
+		
+		}catch (Exception e) {
+			log.log(Level.SEVERE, e.getMessage());
+		}
+		
+		return patientList;
+	}
 }
