b0636c5d4370fa0ef885dae165c5d20e8200a2ae
KaushalyaMadushani <mkaushalya96@gmail.com>
2020-04-16 14:32:16 +0530

    Implemented DB connection class in UserAuth_REST

--- HealthCare Microservices/UserAuth_REST/src/com/caremarque/userAuth/utils/DBConnection.java
+++ HealthCare Microservices/UserAuth_REST/src/com/caremarque/userAuth/utils/DBConnection.java
@@ -7,23 +7,23 @@ import java.sql.SQLException;
 public class DBConnection {
 
 	private static Connection connection;
-	
+
 	private DBConnection() {
-		
+
 	}
-	
+
 	public static Connection getDBConnection() throws ClassNotFoundException, SQLException {
-		
-		if(connection==null ||connection.isClosed()) {
-			
+
+		if (connection == null || connection.isClosed()) {
+
 			Class.forName(Constants.DB_DRIVER_NAME);
-			
-			connection = DriverManager.getConnection(Constants.DBLOCATION_STRING, Constants.DB_USERNAME, Constants.DB_PASSWORD);
-			
+
+			connection = DriverManager.getConnection(Constants.DBLOCATION_STRING, Constants.DB_USERNAME,
+					Constants.DB_PASSWORD);
+
 			System.out.println("Connected to DB");
 		}
-		
 		return connection;
 	}
-	
+
 }
