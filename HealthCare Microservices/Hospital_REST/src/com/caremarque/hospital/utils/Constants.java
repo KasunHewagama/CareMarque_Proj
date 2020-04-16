7d695d2d36b0253cb95cac92d64b628d0a173dba
TaneeshaDew <tdsuwandarachchi98@gmail.com>
2020-04-16 12:17:35 +0530

    ArrayList HospitalID

--- HealthCare Microservices/Hospital_REST/src/com/caremarque/hospital/utils/Constants.java
+++ HealthCare Microservices/Hospital_REST/src/com/caremarque/hospital/utils/Constants.java
@@ -11,17 +11,10 @@ public class Constants {
 	public static final int COLUMN_INDEX_FIVE = 5;
 	public static final int COLUMN_INDEX_SIX = 6;
 	public static final int COLUMN_INDEX_SEVEN = 7;
-	public static final int COLUMN_INDEX_EIGHT = 8;
-	public static final int COLUMN_INDEX_NINE = 9;
-	public static final int COLUMN_INDEX_TEN = 10;
-	public static final int COLUMN_INDEX_ELEVEN = 11;
-	public static final int COLUMN_INDEX_TWELVE = 12;
-	public static final int COLUMN_INDEX_THIRTEEN = 13;
-	public static final int COLUMN_INDEX_FOURTEEN = 14;
-	public static final int COLUMN_INDEX_FIFTEEN = 15;
+
 
 	// ID Prefixes
-	public static final String PAYMENT_ID_PREFIX = "PY";
+	public static final String HOSPITAL_ID_PREFIX = "HID_";
 
 	// DBConstants
 	public static final String DBLOCATION_STRING = "jdbc:mysql://127.0.0.1:3306/healthcare";
