494b3c432735ad54bf0083fd78b9b0cda18a5ffe
TaneeshaDew <tdsuwandarachchi98@gmail.com>
2020-04-18 00:08:57 +0530

    alignment changes

--- HealthCare Microservices/Hospital_REST/src/com/caremarque/hospital/service/IHospitalService.java
+++ HealthCare Microservices/Hospital_REST/src/com/caremarque/hospital/service/IHospitalService.java
@@ -5,17 +5,17 @@ import java.util.ArrayList;
 import com.caremarque.hospital.model.Hospital;
 
 public interface IHospitalService {
-	
+
 	public String createHospital(Hospital hospital);
-	
+
 	public String getHospital(String hospitalId);
-	
+
 	public String getHospitals();
-	
-	public String updateHospital(String hospitalId,Hospital hospital);
-	
+
+	public String updateHospital(String hospitalId, Hospital hospital);
+
 	public String DeleteHospital(String hospitalId);
-	
+
 	public ArrayList<String> getHospitalIDs();
 
 }
