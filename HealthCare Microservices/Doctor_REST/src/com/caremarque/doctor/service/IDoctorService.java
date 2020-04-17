d90af7af035b6f8ab5204b31e8205b867d48185a
Krishmila <madbkjayasinghe97@gmail.com>
2020-04-17 09:15:40 +0530

    add method

--- HealthCare Microservices/Doctor_REST/src/com/caremarque/doctor/service/IDoctorService.java
+++ HealthCare Microservices/Doctor_REST/src/com/caremarque/doctor/service/IDoctorService.java
@@ -1,8 +1,12 @@
 package com.caremarque.doctor.service;
 
+import java.util.ArrayList;
+
 import com.caremarque.doctor.model.Doctor;
 
 public interface IDoctorService {
 
 	public String createDoctor(Doctor doctor);
+
+	public ArrayList<String> getDoctorIDs();
 }
