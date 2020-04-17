74d53ac46a0b4245525f64bee222399e3708681c
Krishmila <madbkjayasinghe97@gmail.com>
2020-04-17 09:34:30 +0530

    cancel doctor

--- HealthCare Microservices/Doctor_REST/src/com/caremarque/doctor/resource/DoctorService.java
+++ HealthCare Microservices/Doctor_REST/src/com/caremarque/doctor/resource/DoctorService.java
@@ -1,6 +1,7 @@
 package com.caremarque.doctor.resource;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -9,6 +10,10 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.parser.Parser;
 
 import com.caremarque.doctor.model.Doctor;
 import com.caremarque.doctor.service.DoctorServiceImpl;
@@ -113,5 +118,22 @@ public class DoctorService {
 		
 		return output;
 	}
+	
+	
+	@DELETE
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_XML)
+	@Produces(MediaType.TEXT_PLAIN)
+	
+	public String deleteDoctor( String doctorData) {
+		Document doc = Jsoup.parse(doctorData,"",Parser.xmlParser());
+		
+		String doctorId = doc.select("doctorId").text();
+		
+		String output = ab.cancelDoctor(doctorId);
+		return output;
+		
+	}
+	
 	
 }
