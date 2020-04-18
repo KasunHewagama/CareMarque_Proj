bbfe28d9cfbde254d07d1bd848d90b272ca86700
KaushalyaMadushani <mkaushalya96@gmail.com>
2020-04-18 14:27:32 +0530

    implement method authenticate in LoginAuthenticationServiceImpl class

--- HealthCare Microservices/UserAuth_REST/src/com/caremarque/userAuth/service/LoginAuthenticationServiceImpl.java
+++ HealthCare Microservices/UserAuth_REST/src/com/caremarque/userAuth/service/LoginAuthenticationServiceImpl.java
@@ -2,6 +2,7 @@ package com.caremarque.userAuth.service;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,7 +10,7 @@ import java.util.logging.Logger;
 import com.caremarque.userAuth.model.loginAuthentication;
 import com.caremarque.userAuth.utils.DBConnection;
 
-public class LoginAuthenticationServiceImpl implements ILoginAuthentication{
+public class LoginAuthenticationServiceImpl {
 
 	private static final Logger logger = Logger.getLogger(LoginAuthenticationServiceImpl.class.getName());
 
@@ -19,29 +20,72 @@ public class LoginAuthenticationServiceImpl implements ILoginAuthentication{
 	
 	private static Statement statement;
 	
-	@Override
-	public List<loginAuthentication> checkLoginCredentials() {
+	
+	public boolean authenticate(String userName, String password, String type){
 		
-		List<loginAuthentication> loginDetailList = new ArrayList<loginAuthentication>();
+		ResultSet rs = null;
+		String email = null;
+		String pwd = null;
 		
 		try {
 			
 			con = DBConnection.getDBConnection();
 			
-			statement = con.createStatement();
-			
-			
-			
+			if(type == "Patient" || type == "patient") {
+				
+				String query = "SELECT email, password FROM patient";
+				
+				statement = con.createStatement();
+				rs = statement.executeQuery(query);
+				
+				while(rs.next()) {
+					
+					 email = rs.getString("email");
+					 pwd = rs.getString("password");
+				}
+				
+				if(email == userName && pwd == password) {
+					return true;
+				}
+				else {
+					return false;
+				}
+							
+			}else if(type == "Doctor" || type == "doctor") {
+				
+				String query = "SELECT email, password FROM doctor";
+				
+				statement = con.createStatement();
+				rs = statement.executeQuery(query);
+				
+				while(rs.next()) {
+					
+					 email = rs.getString("email");
+					 pwd = rs.getString("password");
+				}
+				
+				if(email == userName && pwd == password) {
+					return true;
+				}
+				else {
+					return false;
+				}
+
+			}
 			
 		}catch (Exception e) {
-			// TODO: handle exception
+			
 		}
-		
-		return loginDetailList;
-		
-		
+		return false;
 		
 		
 	}
+	
+	public String issueToken(String userName) {
+		return userName;
+		
+	}
+		
+
 
 }
