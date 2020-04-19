2717fe32fa4f70f466c8d85c65cac67def65088d
YuvinNP <yuvinnimnaka1998@gmail.com>
2020-04-19 23:49:30 +0530

    add patientAuth_REST

---
+++ HealthCare Microservices/PatientAuth_REST/src/com/caremarque/patientAuth/model/PatientAuthentication.java
@@ -0,0 +1,37 @@
+package com.caremarque.patientAuth.model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class PatientAuthentication {
+
+	private int patientAuthId;
+	private String userName;
+
+	public PatientAuthentication() {
+		super();
+	}
+
+	public PatientAuthentication(int patientAuthId, String userName) {
+		super();
+		this.patientAuthId = patientAuthId;
+		this.userName = userName;
+	}
+
+	public int getPatientAuthId() {
+		return patientAuthId;
+	}
+
+	public void setPatientAuthId(int patientAuthId) {
+		this.patientAuthId = patientAuthId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+}
