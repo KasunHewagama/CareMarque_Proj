cf2f9f473e70af65a4bb39ea1b26d4882ba07f83
TaneeshaDew <tdsuwandarachchi98@gmail.com>
2020-04-19 22:32:24 +0530

    DB comments

--- HealthCare Microservices/Hospital_REST/src/com/caremarque/hospital/utils/DBConnection.java
+++ HealthCare Microservices/Hospital_REST/src/com/caremarque/hospital/utils/DBConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
-	// If there is any issue you can change it accordingly
+
 	// Implemented with Singleton Pattern
 
 	private static Connection connection;
